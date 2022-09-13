63638c23b872092802f7f517bba54f47cc7f2cf5
IgorVeto4kin <111492320+IgorVeto4kin@users.noreply.github.com>
2022-09-13 15:54:01 +0300

    L-difference
    разность между чётными и нечётными элементами

--- main.cpp
+++ main.cpp
@@ -2,27 +2,17 @@
 int fib(int q);
 int main()
 {
-    int a;
-    scanf("%d", &a);
-    printf("%d", fib(a));
-    return 0;
-}
-
-int fib( int q) {
-    int l1 = 1, l2 = 1, l3 = 1, l4 = 0;
-    if ( q == 1 || q == 2 || q == 0 ) {
-        return 1;
-    }
-    else {
-        for ( int i = 2 ; i < q ; i++ ) {
-            l4 = l1 + l2 + l3;
-            l1 = l2;
-            l2 = l3;
-            l3 = l4;
-
+    int n = 0, a = 0, sum = 0;
+    scanf("%d", &n);
+    for (int i = 0; i < n ; i++) {
+        scanf("%d", &a);
+        if(i % 2 == 0) {
+            sum += a;
         }
-
-        return l4;
+        else {
+            sum -= a;
+        }
     }
+    printf("%d", sum);
 }
 
