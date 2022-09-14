692201f94c8e7bad21095f62e7910f61aba62f17
IgorVeto4kin <111492320+IgorVeto4kin@users.noreply.github.com>
2022-09-14 16:35:30 +0300

    K-binary
    количество единиц в двоичной заПИСИ числа

--- main.cpp
+++ main.cpp
@@ -1,18 +1,14 @@
 #include <stdio.h>
-int fib(int q);
 int main()
 {
-    int n = 0, a = 0, sum = 0;
-    scanf("%d", &n);
-    for (int i = 0; i < n ; i++) {
-        scanf("%d", &a);
-        if(i % 2 == 0) {
-            sum += a;
+    int a = 0, ans = 0;
+    scanf("%d", &a);
+    while ( a ) {
+        if ( a % 2 == 1 ) {
+            ans+=1;
         }
-        else {
-            sum -= a;
-        }
+        a /= 2 ;
     }
-    printf("%d", sum);
+    printf("%d", ans );
 }
 
